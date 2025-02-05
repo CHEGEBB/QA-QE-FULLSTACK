5e6350082a6002d1da6d52d6cc7cb9917d7bac3c
CHEGEBB <chegephil24@gmail.com>
2025-02-05 14:27:09 +0300

    Refactor package.json to add nodemon for development

---
+++ javascript/1/1.variables/variables.js
@@ -0,0 +1,21 @@
+let age = 25;
+const schoolName = "Greenwood High"
+let studentsList = [];
+
+//What is the difference between let, const, and var when declaring variables?
+//let: keyword when used in declaring variables means that the variable can be reassigned a new value.
+//const: keyword when used in declaring variables means that the variable cannot be reassigned a new value.
+//var: keyword when used in declaring variables means that the variable can be reassigned a new value. It is the old way of declaring variables in JavaScript.
+
+/*
+Which of the following variable names is invalid?
+let $price = 100;
+let 1stPlace = "John";
+let _score = 89;
+let userName = "Alice"; 
+*/
+//valid variable names
+let $price = 100;
+let _score = 89;
+let userName = "Alice";
+
