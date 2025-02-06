21670487da5d098503bfee627f96f3864f667ba5
CHEGEBB <chegephil24@gmail.com>
2025-02-06 12:18:53 +0300

    Refactor code to add a JavaScript function that checks whether an input is a string or not

---
+++ javascript/1/2.strings/strings.js
@@ -0,0 +1,15 @@
+/*Write a JavaScript function to check whether an 'input' is a string or not.
+Test Data:
+console.log(is_string('w3resource')); // true
+○ console.log(is_string([1, 2, 4, 0])); // false
+*/
+
+function is_string (input){
+    if(typeof input === 'string'){
+        return true;
+    } else {
+        return false;
+    }
+}
+console.log(is_string('w3resource'));
+console.log(is_string([1, 2, 4, 0])); 
