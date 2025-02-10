18106bc926837e78fd0401db7fd1e6837a0fe5d1
CHEGEBB <chegephil24@gmail.com>
2025-02-10 20:25:25 +0300

    Refactor code to add palindrome, anagram, and duplicate character removal functions

--- javascript/1/4.LogicalOperators/assignment2/index.js
+++ javascript/1/4.LogicalOperators/assignment2/index.js
@@ -36,4 +36,48 @@ function longestPalindromicSubstring(s) {
 console.log(longestPalindromicSubstring('babad')); // 'bab' or 'aba'
 console.log(longestPalindromicSubstring('cbbd'));  // 'bb'
 
+/*Write a function to check if two given strings are anagrams of each other. 
+Two strings are anagrams if they contain the same characters in the same frequency but in different orders. */
+function areAnagrams( str1, str2 ) {
+    let first = str1.toLowerCase().split("").sort();
+    let second = str2.toLowerCase().split("").sort();
+
+     if (first.length !== second.length) return false;
+    
+    for (let i = 0; i < first.length; i++) {
+        if (first[i] !== second[i]) return false;
+    }
+    
+    return true;
+
+}
+console.log(areAnagrams('Listen', 'Silent'));
+console.log(areAnagrams('Hello' , 'World'));
+
+/*Write a function to remove duplicate characters from a string while preserving the order of the first appearance of each character. */
+
+function removeDuplicates(str) {
+    let result = '';
+    let string = [];
+
+    for (let i = 0; i < str.length; i++) {
+        if (!string[str[i]]) {
+            result += str[i];
+            string[str[i]] = true;
+        }
+    }
+    
+    return result;
+}
+
+console.log(removeDuplicates('programming')); 
+console.log(removeDuplicates('hello world'));
+console.log(removeDuplicates('aaaaa')); 
+console.log(removeDuplicates('abcd')); 
+console.log(removeDuplicates('aabbcc')); 
+
+
+
+
+
 
